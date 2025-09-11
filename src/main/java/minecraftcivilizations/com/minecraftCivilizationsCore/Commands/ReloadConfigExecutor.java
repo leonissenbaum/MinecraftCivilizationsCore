0f6af61b9de360d4562e575d8161b58b81e39928
bananababoo <banababnana@gmail.com>
2025-09-10 23:13:15 -0500

    fix perms

--- src/main/java/minecraftcivilizations/com/minecraftCivilizationsCore/Commands/ReloadConfigExecutor.java
+++ src/main/java/minecraftcivilizations/com/minecraftCivilizationsCore/Commands/ReloadConfigExecutor.java
@@ -2,15 +2,21 @@ package minecraftcivilizations.com.minecraftCivilizationsCore.Commands;
 
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
 import minecraftcivilizations.com.minecraftCivilizationsCore.Config.ConfigFilesManager;
 import minecraftcivilizations.com.minecraftCivilizationsCore.MinecraftCivilizationsCore;
+import org.bukkit.entity.Player;
 
 @CommandAlias("reloadconfig")
+@CommandPermission("core.reload")
 public class ReloadConfigExecutor extends BaseCommand {
 
     @Default
-    public void onReloadConfig() {
-        ConfigFilesManager.reloadConfigFiles();
+    @CommandPermission("core.reload")
+    public void onReloadConfig(Player player) {
+        if(player.isOp()) {
+            ConfigFilesManager.reloadConfigFiles();
+        }
     }
 }
